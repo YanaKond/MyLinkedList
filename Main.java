e3ddca88349d78a5c4854d44be1f5768647e6cae
Yana Kond <https://github.com/YanaKond>
2024-03-19 20:40:54 +0300

    add changes

--- Main.java
+++ Main.java
@@ -5,13 +5,11 @@ public class Main {
         list.add(2);
         list.add(3);
 
-        // Перебор элементов с помощью цикла for-each
         System.out.println("Элементы в списке:");
         for (Integer element : list) {
             System.out.println(element);
         }
 
-        // Удаление элемента
         list.remove(2);
         System.out.println("\nПосле удаления элемента 2:");
         for (Integer element : list) {
